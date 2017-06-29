88cf1b8f54e9cfa0a49ff04338b75914a4d6466a
VooDooS <thevoodoos@gmail.com>
2017-06-29 15:16:50 +0200

    Mainactivity (list) not restarted every time !

--- app/src/main/java/fr/u31/rythm/AbstractTrainActivity.java
+++ app/src/main/java/fr/u31/rythm/AbstractTrainActivity.java
@@ -17,6 +17,8 @@ import android.util.Pair;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
